962b6006db25b5b13e94b48393bc76a16af69c24
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-07-04 09:18:29 -0500

    subir informacion

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class HolaMundo {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HolaMundo {
+
+    private JPanel rootPanel;
+    private JButton okButton;
+    private JLabel resultado;
+    private JTextField numero1;
+    private JTextField numero2;
+    int i=0;
+
+    public HolaMundo() {
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //resultado.setText("Presionaste el boton OK");
+                i++;
+                //convierte un entero a string Integer.toString(i)
+                //resultado.setText(Integer.toString(i));
+                resultado.setText(numero1.getText()+numero2.getText()+Integer.toString(i));
+                numero1.getText();
+                if (i==3){
+                    resultado.setText("Has presionado el boton 3 veces");
+                    System.out.println("Has presionado el boton 3 veces");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HolaMundo");
+        frame.setContentPane(new HolaMundo().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
 }
